2897cacfd0de9e29690c199d2ed6efc7d3365e4d
Matt Rose <rose.matt@gmail.com>
2015-05-19 11:30:31 -0400

    Fixed pointers to touch helpers

--- test/gemini-carousel.test.js
+++ test/gemini-carousel.test.js
@@ -44,6 +44,10 @@ requirejs.config({
     'gemini':                    'bower_components/gemini-loader/gemini',
     'gemini.fold':               'bower_components/gemini-fold/gemini.fold',
     'gemini.respond':            'bower_components/gemini-respond/gemini.respond',
+    'gemini.touch':              'bower_components/gemini-touch/gemini.touch',
+    'fastclick':                 'bower_components/fastclick/lib/fastclick',
+    'hammerjs':                  'bower_components/hammerjs/hammer',
+    'jquery.hammer':             'bower_components/jquery-hammerjs/jquery.hammer',
     'gemini.carousel.templates': 'templates'
   }
 });
