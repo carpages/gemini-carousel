298f04c19712be3b4a1fa33a437e7a65ff1d33c3
Jeff Andrews <jeff.andrews@carpages.ca>
2017-03-22 15:46:24 -0400

    Handlebars v4

--- templates.js
+++ templates.js
@@ -20,11 +20,11 @@ this["Templates"] = this["Templates"] || {};
 this["Templates"]["Default"] = this["Templates"]["Default"] || {};
 this["Templates"]["Default"]["Carousel"] = this["Templates"]["Default"]["Carousel"] || {};
 
-this["Templates"]["Default"]["Carousel"]["nav"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["Templates"]["Default"]["Carousel"]["nav"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper;
 
   return "<ol class=\"carousel__nav pagination pagination--carousel\">\n\n 	<li class=\"pagination__prev\"><a href=\"#\" data-goto=\"--\">Previous</a></li>\n\n	<li class=\"pagination__item\">\n		<span class=\"carousel__current-page-count\">1</span>\n		/\n		<span class=\"carousel__page-count\">"
-    + this.escapeExpression(((helper = (helper = helpers.pageCount || (depth0 != null ? depth0.pageCount : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"pageCount","hash":{},"data":data}) : helper)))
+    + container.escapeExpression(((helper = (helper = helpers.pageCount || (depth0 != null ? depth0.pageCount : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"pageCount","hash":{},"data":data}) : helper)))
     + "</span>\n	</li>\n\n	<li class=\"pagination__next\"><a href=\"#\" data-goto=\"++\">Next</a></li>\n\n</ol>\n";
 },"useData":true});
 
