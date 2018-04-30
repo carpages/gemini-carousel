ac99dd9b9b5f2e722adc4244d6b2331aee9194a6
Jeff Andrews <jeff.andrews@carpages.ca>
2018-04-30 09:09:16 -0400

    Add opt-in incrementByOne functionality when multiple items per page.

--- gemini.carousel.js
+++ gemini.carousel.js
@@ -26,6 +26,7 @@ You can see this in the example
  * @requires gemini.support
 
  * @prop {boolean} pagination {@link gemini.carousel#pagination}
+ * @prop {boolean} incrementByOne {@link gemini.carousel#incrementByOne}
  * @prop {boolean} loop {@link gemini.carousel#loop}
  * @prop {boolean} animate {@link gemini.carousel#animate}
  * @prop {string} container {@link gemini.carousel#container}
@@ -56,17 +57,15 @@ You can see this in the example
  */
 
 define([
-    'gemini',
-    'gemini.carousel.templates',
-    'gemini.fold',
-    'gemini.respond',
-    'gemini.support'
-  ], function($, T){
-
+  'gemini',
+  'gemini.carousel.templates',
+  'gemini.fold',
+  'gemini.respond',
+  'gemini.support'
+], function( $, T ) {
   var _ = $._;
 
-  $.boiler('carousel', {
-
+  $.boiler( 'carousel', {
     defaults: {
       /**
        * Whether to append pagination to the the carousel.
@@ -75,6 +74,14 @@ define([
        * @default false
        */
       pagination: false,
+
+      /**
+       * Set to true to increment by one item regardless of how many are visible.
+       * @name gemini.carousel#incrementByOne
+       * @type Boolean
+       * @default false
+       */
+      incrementByOne: false,
 
       /**
        * Whether to do a scroll animation when clicked
@@ -150,64 +157,65 @@ define([
     },
 
     // Initiate the plugin
-    init: function(){
+    init: function() {
       var P = this;
 
-      //Extend the templates
-      P.T = $.extend(T, P.settings.templates);
+      // Extend the templates
+      P.T = $.extend( T, P.settings.templates );
 
       // Init variables
       P.currentItem = P.currentPage = 1;
 
       // Cache jQuery objects
-      P.$carousel = !!P.settings.container ?
-                    P.$el.find(P.settings.container) :
-                    P.$el;
-      P.$carouselLists = P.$el.find('.carousel__list');
+      P.$carousel = P.settings.container
+        ? P.$el.find( P.settings.container )
+        : P.$el;
+      P.$carouselLists = P.$el.find( '.carousel__list' );
       P.carouselList = P.$carouselLists[P.settings.indexList];
-      P.$carouselList = $(P.carouselList);
-      P.$currentPage = P.$el.find('[data-goto="1"]');
-      P.$next = P.$el.find('[data-goto="next"],[data-goto="++"]');
-      P.$previous = P.$el.find('[data-goto="prev"],[data-goto="previous"],[data-goto="--"]');
+      P.$carouselList = $( P.carouselList );
+      P.$currentPage = P.$el.find( '[data-goto="1"]' );
+      P.$next = P.$el.find( '[data-goto="next"],[data-goto="++"]' );
+      P.$previous = P.$el.find(
+        '[data-goto="prev"],[data-goto="previous"],[data-goto="--"]'
+      );
 
-      //Change next/prev button active states
-      if(!P._isNext()) {
-        P.$next.addClass('is-inactive');
+      // Change next/prev button active states
+      if ( !P._isNext()) {
+        P.$next.addClass( 'is-inactive' );
       } else {
-        P.$next.removeClass('is-inactive');
+        P.$next.removeClass( 'is-inactive' );
       }
-      if(!P._isPrevious()) {
-        P.$previous.addClass('is-inactive');
+      if ( !P._isPrevious()) {
+        P.$previous.addClass( 'is-inactive' );
       } else {
-        P.$previous.removeClass('is-inactive');
+        P.$previous.removeClass( 'is-inactive' );
       }
 
       // Update
       P._update();
 
       // Update on resize
-      $.respond.bind('resize', function(e, scrn){
+      $.respond.bind( 'resize', function( e, scrn ) {
         P._update();
-        P.gotoPage(P.currentPage, false);
+        P.gotoPage( P.currentPage, false );
       });
 
       // Setup thumbnails
-      if (P.settings.thumbs) {
-        P.$thumbs = P.$el.find('.carousel__thumbs');
-        P.$thumbs.find('a').each(function(i){
-          $(this).click(function(e){
+      if ( P.settings.thumbs ) {
+        P.$thumbs = P.$el.find( '.carousel__thumbs' );
+        P.$thumbs.find( 'a' ).each( function( i ) {
+          $( this ).click( function( e ) {
             e.preventDefault();
-            P.gotoPage(i + 1);
+            P.gotoPage( i + 1 );
           });
         });
       }
 
       // Touch Support
-      if($.support.touch) {
+      if ( $.support.touch ) {
         P.settings.animate = true;
         P._initTouch();
       }
-
     },
 
     /**
@@ -216,19 +224,21 @@ define([
      * @private
      * @function
      */
-    _update: function(){
+    _update: function() {
       var P = this;
 
-      //Update Cache
+      // Update Cache
       P.pageWidth = P.$carouselList.width();
-      P.itemWidth = P.$carouselList.children('li:first-child').width();
-      P.itemsPerPage = Math.round(P.pageWidth/P.itemWidth);
+      P.itemWidth = P.$carouselList.children( 'li:first-child' ).width();
 
-      P.itemCount = P.$carouselList.children('li').length;
+      P._itemsPerPage = Math.round( P.pageWidth / P.itemWidth );
+      P.itemsPerPage = P.settings.incrementByOne ? 1 : P._itemsPerPage;
+
+      P.itemCount = P.$carouselList.children( 'li' ).length;
       P.pageCount = Math.ceil( P.itemCount / P.itemsPerPage );
 
       // Template the pagination
-      if(P.settings.pagination){
+      if ( P.settings.pagination ) {
         P._paginate();
       }
     },
@@ -239,26 +249,26 @@ define([
      * @private
      * @function
      */
-    _paginate: function(){
+    _paginate: function() {
       var P = this;
 
-      //Template
+      // Template
       P.pagination = P.T.nav({
-        'pageCount': P.pageCount
+        pageCount: P.pageCount
       });
 
-      //Render
-      if(!P.$pagination){
-        //First load
-        P.$pagination = $('<span/>').append(P.pagination);
-        P.$el.append(P.$pagination);
+      // Render
+      if ( !P.$pagination ) {
+        // First load
+        P.$pagination = $( '<span/>' ).append( P.pagination );
+        P.$el.append( P.$pagination );
       } else {
-        //Replace exisiting
-        P.$pagination.html(P.pagination);
+        // Replace exisiting
+        P.$pagination.html( P.pagination );
       }
 
-      //Cache
-      P.$currentPageCount = P.$pagination.find('.carousel__current-page-count');
+      // Cache
+      P.$currentPageCount = P.$pagination.find( '.carousel__current-page-count' );
     },
 
     /**
@@ -269,21 +279,21 @@ define([
      * @param {event#object} e Click event object
      * @param {element} target The targeted element
      */
-    _handleClick: function(e, target){
+    _handleClick: function( e, target ) {
       var P = this;
-      var goTo = $(target).data('goto');
+      var goTo = $( target ).data( 'goto' );
 
-      if(!goTo) return;
+      if ( !goTo ) return;
 
       e.preventDefault();
-      //Next
-      if(goTo == "next" || goTo == "++")
-        P.next();
-      //Previous
-      else if(goTo == "prev" || goTo == "previous" || goTo == "--")
+      // Next
+      if ( goTo == 'next' || goTo == '++' ) P.next();
+      // Previous
+      else if ( goTo == 'prev' || goTo == 'previous' || goTo == '--' ) {
         P.previous();
-      //Go to page
-      else P.gotoPage(goTo);
+      }
+      // Go to page
+      else P.gotoPage( goTo );
     },
 
     /**
@@ -293,7 +303,7 @@ define([
      * @function
      * @return {boolean} Weather the next page exists
      */
-    _isNext: function(){
+    _isNext: function() {
       return this.settings.loop || this.currentPage != this.pageCount;
     },
 
@@ -304,7 +314,7 @@ define([
      * @function
      * @return {boolean} Weather the previous page exists
      */
-    _isPrevious: function(){
+    _isPrevious: function() {
       return this.settings.loop || this.currentPage != 1;
     },
 
@@ -313,9 +323,9 @@ define([
      * @name gemini.carousel#next
      * @function
      */
-    next: function(){
+    next: function() {
       var P = this;
-      if(P._isNext()) P.gotoPage(P.currentPage + 1);
+      if ( P._isNext()) P.gotoPage( P.currentPage + 1 );
     },
 
     /**
@@ -323,9 +333,9 @@ define([
      * @name gemini.carousel#previous
      * @function
      */
-    previous: function(){
+    previous: function() {
       var P = this;
-      if(P._isPrevious()) P.gotoPage(P.currentPage - 1);
+      if ( P._isPrevious()) P.gotoPage( P.currentPage - 1 );
     },
 
     /**
@@ -334,45 +344,50 @@ define([
      * @private
      * @function
      */
-    _gotoItem: function(item, animate){
-      if(animate===undefined) animate = true;
+    _gotoItem: function( item, animate ) {
+      if ( animate === undefined ) animate = true;
 
       var P = this;
-      if(item > P.itemCount) return;
+      if ( item > P.itemCount ) return;
 
-      //Calculate the x offset in pixels
-      var $item = P.$carouselList.children('li:nth-child('+item+')'),
-          xOffset = $item.offset().left -
-                    P.$carouselList.offset().left +
-                    P.carouselList.scrollLeft;
+      // Calculate the x offset in pixels
+      var $item = P.$carouselList.children( 'li:nth-child(' + item + ')' ),
+        xOffset =
+          $item.offset().left -
+          P.$carouselList.offset().left +
+          P.carouselList.scrollLeft;
 
-      //Whether there are more items to scroll to
-      var isMoreItems = $.rightoffold(
-        P.$carouselList.find('li').last(),{
-          container:P.$carouselList,
-          threshold:-P.itemWidth-20
-        }
-      );
+      // Whether there are more items to scroll to
+      var isMoreItems = $.rightoffold( P.$carouselList.find( 'li' ).last(), {
+        container: P.$carouselList,
+        threshold: -P.itemWidth - 20
+      });
 
-      //Make sure there's something to scroll to
-      if ((item > P.currentItem && !isMoreItems) || xOffset < 0) return;
+      // Make sure there's something to scroll to
+      if (( item > P.currentItem && !isMoreItems ) || xOffset < 0 ) return;
 
-      //Change the item
+      // Change the item
       P.currentItem = item;
-      if(animate){
-        P.$carouselLists.animate({
-          scrollLeft:xOffset
-        }, P.settings.scrollSpeed);
-      }else{
-        P.$carouselLists.scrollLeft(xOffset);
+      P.allItemsShown =
+        P.currentItem - 1 === P.itemCount - ( P._itemsPerPage - 1 );
+      if ( animate ) {
+        P.$carouselLists.animate(
+          {
+            scrollLeft: xOffset
+          },
+          P.settings.scrollSpeed
+        );
+      } else {
+        P.$carouselLists.scrollLeft( xOffset );
       }
 
       setTimeout(
-        _.bind(function(){
-          $(this).trigger("scroll");
-          if(P.settings.onChange) P.settings.onChange.call(P);
-        }, P.$carousel),
-      animate ? P.settings.scrollSpeed: 0);
+        _.bind( function() {
+          $( this ).trigger( 'scroll' );
+          if ( P.settings.onChange ) P.settings.onChange.call( P );
+        }, P.$carousel ),
+        animate ? P.settings.scrollSpeed : 0
+      );
     },
 
     /**
@@ -381,40 +396,43 @@ define([
      * @function
      * @param {Integer} page The desired page
      */
-    gotoPage: function(page, animate){
+    gotoPage: function( page, animate ) {
       var P = this;
 
-      if(animate===undefined) animate = P.settings.animate;
+      if ( animate === undefined ) animate = P.settings.animate;
 
-      if(page > P.pageCount) {
-        if (P.settings.loop) P.gotoPage(1);
+      if ( P.allItemsShown || page > P.pageCount ) {
+        if ( P.settings.loop ) {
+          P.allItemsShown = false;
+          P.gotoPage( 1 );
+        }
         return;
-      } else if (page < 1) {
-        if (P.settings.loop) P.gotoPage(P.pageCount);
+      } else if ( page < 1 ) {
+        if ( P.settings.loop ) P.gotoPage( P.pageCount );
         return;
       }
 
-      var item = P.itemsPerPage * (page - 1) + 1;
+      var item = P.itemsPerPage * ( page - 1 ) + 1;
 
-      P._gotoItem(item, animate);
+      P._gotoItem( item, animate );
 
-      //Change active page
+      // Change active page
       P.currentPage = page;
-      P.$currentPage.removeClass('is-active');
-      P.$currentPage = P.$el.find('[data-goto="'+P.currentPage+'"]');
-      P.$currentPage.addClass('is-active');
-      if(!!P.$currentPageCount) P.$currentPageCount.html(page);
+      P.$currentPage.removeClass( 'is-active' );
+      P.$currentPage = P.$el.find( '[data-goto="' + P.currentPage + '"]' );
+      P.$currentPage.addClass( 'is-active' );
+      if ( P.$currentPageCount ) P.$currentPageCount.html( page );
 
-      //Change next/prev button active states
-      if(!P._isNext()) {
-        P.$next.addClass('is-inactive');
+      // Change next/prev button active states
+      if ( !P._isNext()) {
+        P.$next.addClass( 'is-inactive' );
       } else {
-        P.$next.removeClass('is-inactive');
+        P.$next.removeClass( 'is-inactive' );
       }
-      if(!P._isPrevious()) {
-        P.$previous.addClass('is-inactive');
+      if ( !P._isPrevious()) {
+        P.$previous.addClass( 'is-inactive' );
       } else {
-        P.$previous.removeClass('is-inactive');
+        P.$previous.removeClass( 'is-inactive' );
       }
     },
 
@@ -424,67 +442,68 @@ define([
      * @private
      * @function
      */
-    _initTouch: function(){
+    _initTouch: function() {
       var P = this;
 
-      require(['gemini.touch'], function(){
+      require([ 'gemini.touch' ], function() {
+        // Add touch events
+        P.$carouselList
+          .hammer({
+            dragBlockHorizontal: true,
+            dragLockToAxis: true,
+            dragLockMinDistance: 20,
+            hold: false,
+            tap: false
+          })
+          .on( 'release dragleft dragright', function( ev ) {
+            switch ( ev.type ) {
+              case 'dragright':
+              case 'dragleft':
+                // stick to the finger
+                var pageOffset = ( P.currentPage - 1 ) * P.pageWidth;
+                var dragOffset = -ev.gesture.deltaX;
 
-        //Add touch events
-        P.$carouselList.hammer({
-          dragBlockHorizontal: true,
-          dragLockToAxis: true,
-          dragLockMinDistance: 20,
-          hold: false,
-          tap: false
-        }).on('release dragleft dragright', function(ev){
-
-          switch(ev.type) {
-            case 'dragright':
-            case 'dragleft':
-              // stick to the finger
-              var pageOffset = (P.currentPage-1)*P.pageWidth;
-              var dragOffset = -ev.gesture.deltaX;
-
-              // slow down at the first and last pane
-              if((P.currentPage == 1 && ev.gesture.direction == "right") ||
-                (P.currentPage == P.pageCount && ev.gesture.direction == "left")) {
-                dragOffset *= 0.4;
-              }
-
-              P.$carouselLists.scrollLeft(pageOffset + dragOffset);
-              break;
-
-            case 'release':
-              // check if their finger is moving fast
-              if (ev.gesture.velocityX > 0.05) {
-                if (ev.gesture.interimDirection == "left") {
-                  P.next();
-                } else if (ev.gesture.interimDirection == "right") {
-                  P.previous();
+                // slow down at the first and last pane
+                if (
+                  ( P.currentPage == 1 && ev.gesture.direction == 'right' ) ||
+                  ( P.currentPage == P.pageCount &&
+                    ev.gesture.direction == 'left' )
+                ) {
+                  dragOffset *= 0.4;
                 }
-              // snap carousel base on positioning of page
-              } else {
-                // more then 50% moved, navigate
-                if (Math.abs(ev.gesture.deltaX) > P.pageWidth/2) {
-                  if (ev.gesture.direction == 'right') {
-                    P.previous();
-                  } else {
+
+                P.$carouselLists.scrollLeft( pageOffset + dragOffset );
+                break;
+
+              case 'release':
+                // check if their finger is moving fast
+                if ( ev.gesture.velocityX > 0.05 ) {
+                  if ( ev.gesture.interimDirection == 'left' ) {
                     P.next();
+                  } else if ( ev.gesture.interimDirection == 'right' ) {
+                    P.previous();
                   }
+                  // snap carousel base on positioning of page
                 } else {
-                  P.gotoPage(P.currentPage);
+                  // more then 50% moved, navigate
+                  if ( Math.abs( ev.gesture.deltaX ) > P.pageWidth / 2 ) {
+                    if ( ev.gesture.direction == 'right' ) {
+                      P.previous();
+                    } else {
+                      P.next();
+                    }
+                  } else {
+                    P.gotoPage( P.currentPage );
+                  }
                 }
-              }
-              break;
+                break;
             }
-        });
+          });
       });
     }
-
   });
 
   // Return the jquery object
   // This way you don't need to require both jquery and the plugin
   return $;
-
 });
